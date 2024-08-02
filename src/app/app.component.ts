560ccda4c601393e78abd8cde2f0e779cdbcde22
Arkalithe <arkalithe@gmail.com>
2024-08-02 18:53:09 +0200

    Styling with Materialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component, OnInit } from "@angular/core";
+import { POKEMONS } from "./mock-pokemon-list";
+import { Pokemon } from "./pokemon";
 
 @Component({
-  selector: 'app-root',
-  templateUrl:'app.component.html'
-})  
-export class AppComponent  implements OnInit{
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrl: "app.component.css",
+})
+export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
+  pokemonSelected: Pokemon | undefined;
 
-constructor () {
-}
+  constructor() {}
 
   ngOnInit(): void {
-    console.table(this.pokemonList);     
+    console.table(this.pokemonList);
   }
 
   selectPokemon(pokemonId: String) {
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
-    if(pokemon) {
-          console.log(`Vous avez slelectionner le pokemon : ${pokemon.name}
+    const pokemon: Pokemon | undefined = this.pokemonList.find(
+      (pokemon) => pokemon.id == +pokemonId
+    );
+    if (pokemon) {
+      console.log(`Vous avez slelectionner le pokemon : ${pokemon.name}
       Il a a : ${pokemon.hp} points de vie`);
       this.pokemonSelected = pokemon;
     } else {
       console.log(`Vous avez demander un pokemon qui n'existe pas `);
-        this.pokemonSelected = pokemon;
+      this.pokemonSelected = pokemon;
     }
-    
-
-    
-  } ;
+  }
 }
