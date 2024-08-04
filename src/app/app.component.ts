0157a9942376ef4cab0933d6b018a7754c526c62
Arkalithe <arkalithe@gmail.com>
2024-08-04 11:58:01 +0200

    Implementation des router et des page List pokemon, detail pokemon et page not found

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { POKEMONS } from "./mock-pokemon-list";
-import { Pokemon } from "./pokemon";
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
   styleUrl: "app.component.css",
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
-
-  constructor() {}
-
-  ngOnInit(): void {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: String) {
-    const pokemon: Pokemon | undefined = this.pokemonList.find(
-      (pokemon) => pokemon.id == +pokemonId
-    );
-    if (pokemon) {
-      console.log(`Vous avez slelectionner le pokemon : ${pokemon.name}
-      Il a a : ${pokemon.hp} points de vie`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez demander un pokemon qui n'existe pas `);
-      this.pokemonSelected = pokemon;
-    }
-  }
-}
+export class AppComponent {}
--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -7,15 +7,12 @@ export class BorderCardDirective {
   private initalColor: string = "#f5f5f5";
   private defaultColor: string = "#009688";
   private defaultHeight: number = 200;
-  private showPopupNameOnClick: boolean = true;
-
   constructor(private el: ElementRef) {
     this.setHeight(this.defaultHeight);
     this.setBorder(this.initalColor);
   }
 
   @Input("pokemonBorderCard") borderColor: string;
-  @Input("pokemonName") pokemon: any;
 
   @HostListener("mouseenter") onMouseEnter() {
     this.setBorder(this.borderColor || this.defaultColor);
@@ -25,15 +22,6 @@ export class BorderCardDirective {
     this.setBorder(this.initalColor);
   }
 
-  @HostListener("click") onClick() {
-    if (this.showPopupNameOnClick) {
-      this.showPopupName();
-    }
-  }
-
-  private showPopupName() {
-    alert(`Le nom du Pokémon est ${this.pokemon?.name}`);
-  }
   private setHeight(height: number) {
     this.el.nativeElement.style.height = `${height}px`;
   }
