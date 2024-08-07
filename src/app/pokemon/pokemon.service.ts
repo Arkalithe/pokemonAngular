7e0d6babe3658616f961ccc22fd1e98f921cd447
Vigneron tristan <arkalithe@gmail.com>
2024-08-07 10:51:39 +0200

    debut service

---
+++ src/app/pokemon/pokemon.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { POKEMONS } from "./mock-pokemon-list";
+import { Pokemon } from "./pokemon";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PokemonService {
+  getPokemonList(): Pokemon[] {
+    return POKEMONS;
+  }
+  getPokemonById(pokemonId: number): Pokemon | undefined {
+    return POKEMONS.find((pokemon) => pokemon.id == pokemonId);
+  }
+
+  getPokemonType(): string[] {
+    return [
+      "Plante",
+      "Feu",
+      "Eau",
+      "Insecte",
+      "Normal",
+      "Electrik",
+      "Poison",
+      "Fée",
+      "Vol",
+      "Combat",
+      "Psy",
+    ];
+  }
+}
